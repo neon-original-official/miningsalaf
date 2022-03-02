e1b646e05b221723f94aa293065f983869e9b6aa
neon-original-official <83065241+neon-original-official@users.noreply.github.com>
2022-03-02 16:33:35 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-sudo apt upgrade
-sudo apt update
+apt upgrade
+apt update
 echo Loading Pacakage
-sudo apt-get install -y wget
-sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-sudo apt install -y --fix-broken
+apt-get install -y wget
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+dpkg --install chrome-remote-desktop_current_amd64.deb
+apt install -y --fix-broken
 echo Finishing...
 DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AX4XfWh3DuxUlXwRbVE5y-GHNwzNwldfOI4fvSVRMHuFlffSf4FRSsxLzPcAL187xRVVAA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
